75962593dec6289a5b38f3684e8e8c420f627966
Sean Huni <sean2kay@gmail.com>
2020-06-07 15:59:25 +0200

    Fixed init Param-Syntax Error.

--- rest/switch.py
+++ rest/switch.py
@@ -39,7 +39,7 @@ class HealthCheck(Resource):
 
 class StrMsg:
     @staticmethod
-    def getStrDate(self) -> str:
+    def getStrDate() -> str:
         lDateTime = dumps(dt.datetime.now(), indent=4, sort_keys=True, default=str)
         resp = str(lDateTime).strip('"')
         return resp
