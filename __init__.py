36932e2ba74a88c2bda47700843c7e033ac6b76f
Sean Huni <sean2kay@gmail.com>
2020-06-07 15:34:13 +0200

    Fixed Execution-Sequence.

--- __init__.py
+++ __init__.py
@@ -19,15 +19,14 @@ class Main:
 if __name__ == '__main__':
     main = Main()
     try:
-        VoiceCmd('{0}: System Startup sequence in execution'.format(str(main.getStrDate())),
-                 'command/sp/speech_system-startup.mp3')
+        VoiceCmd('{0}: System Startup sequence in execution'.format(str(main.getStrDate())), 'command/sp/speech_system-startup.mp3')
         Config.config()
         api.add_resource(HealthCheck, '/api/v1/host')  # Route_1
         api.add_resource(ToggleSwitch, '/api/v1/devices/<device>')  # Route_2
         api.app.debug = True
+        VoiceCmd('{0}: System Initialisation Completed!!!'.format(str(main.getStrDate())), 'command/sp/speech_system-init-complete.mp3')
         api.app.run(host='0.0.0.0', port='8083')
-        VoiceCmd('{0}: System Initialisation Completed!!!'.format(str(main.getStrDate())),
-                 'command/sp/speech_system-init-complete.mp3')
+        Config.clear_up()
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the flowing code will be executed.
         print('{0}: Graceful Termination'.format(str(main.getStrDate())))
         Config.clear_up()
