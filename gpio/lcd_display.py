907f53aaf1db04ddfcc17aa6a8e4e28ca7db4183
Sean Huni <sean2kay@gmail.com>
2020-06-09 00:11:31 +0200

    trying out a fix

--- gpio/lcd_display.py
+++ gpio/lcd_display.py
@@ -23,11 +23,11 @@ lcd_rows = 2
 
 class LcdDevice:
     x = False
-    lcd = LCD()
+    lcd = None
 
     def __init__(self):
         # Initialize the LCD using the pins above.
-        self.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+        self.lcd = LCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
         print('{0}: LCD Instance Created!!!'.format(Utility.getStrDate()))
 
     def display_msg(self, msg):
