aa10c6bc1002fa9396b6f2490b5d9cef9c04deab
Sean Huni <sean2kay@gmail.com>
2020-06-09 12:29:10 +0200

    Fixed Degree Celsius Symbol

--- gpio/lcd_display.py
+++ gpio/lcd_display.py
@@ -53,7 +53,7 @@ class LcdDevice:
             gpu = re.findall(r'[-+]?\d*\.?\d+|[-+]?\d+', gpu)
             cpu = float(cpu[0]) / 1000
             gpu = float(gpu[0])
-            print('{0}: CPU: {1:.2f}{3}C\nGPU: {2:.2f}{3}C'.format(Utility.getStrDate(), cpu, gpu, chr(223)))
+            print('{0}: CPU: {1:.2f}{3}C\nGPU: {2:.2f}{3}C'.format(Utility.getStrDate(), cpu, gpu, '°'))
 
             self.lcd.clear()
             self.lcd.message('CPU: {0:.2f}{2}C\nGPU: {1:.2f}{2}C'.format(cpu, gpu, chr(223)))
